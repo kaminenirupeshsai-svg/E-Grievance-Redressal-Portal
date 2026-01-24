3a3cde89122321f96c53bb41c69dd8e01d70ff37
kaminenirupeshsai-svg <kaminenirupeshsai@gmail.com>
2026-01-24 21:46:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 
 /* ===============================
    1. BODY PARSERS
@@ -68,5 +69,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 Server running on port ${PORT}`)
 );
+
 
 
