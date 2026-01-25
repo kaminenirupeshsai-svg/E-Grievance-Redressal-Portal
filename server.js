2f4ac95f744e76e703439e61adbcde1abbeb25db
kaminenirupeshsai-svg <kaminenirupeshsai@gmail.com>
2026-01-25 12:35:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,11 +35,12 @@ app.use(
     secret: process.env.SESSION_SECRET || "super-secret-key",
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({
+    store: MongoStore.create({
       mongoUrl: process.env.MONGO_URL,
     }),
   })
 );
+
 
 
 /* ===============================
@@ -69,6 +70,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 Server running on port ${PORT}`)
 );
+
 
 
 
