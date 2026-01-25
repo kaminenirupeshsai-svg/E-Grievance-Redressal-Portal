a5a54ee629d16d31067ae74130c18cab08264f3b
kaminenirupeshsai-svg <kaminenirupeshsai@gmail.com>
2026-01-25 12:51:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
+const MongoStore = require("connect-mongo").default;
 const path = require("path");
 require("dotenv").config();
 
@@ -70,6 +70,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 Server running on port ${PORT}`)
 );
+
 
 
 
