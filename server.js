24051fc519f43021bb921dc061dbeca10cbdebf9
kaminenirupeshsai-svg <kaminenirupeshsai@gmail.com>
2026-01-26 10:19:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,9 +67,11 @@ app.get("/health", (req, res) => res.send("✅ Server running"));
    8. START SERVER
 ================================ */
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on port ${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
+
 
 
 
